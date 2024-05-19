ae2865d90d530bd4847c2f467aab281261930a34
welecomtopython <johndoe@example.com>
2024-05-19 03:05:06 +0300

    push express

---
+++ api/index.js
@@ -0,0 +1,13 @@
+// server.js
+
+const express = require("express");
+const app = express();
+
+// A simple get greet method
+app.get("/greet", (req, res) => {
+  // get the passed query
+  const { name } = req.query;
+  res.send({ msg: `Welcome ${name}!` });
+});
+app.use(express.static("public"));
+app.listen()
